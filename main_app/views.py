87a7f0e297a22ef876a6a1677231b73f7fafdfa7
Alyssa Alonso <aalonso310@gmail.com>
2023-11-28 16:50:25 -0500

    modify views

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Bills, User, Income, Expenses, FinancialHealth
 from .finhealth import finhealth_index
 from .models import Bill, User, Income, Expenses, FinancialHealth
 from .forms import UserForm
