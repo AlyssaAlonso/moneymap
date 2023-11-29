63c4cccc93305f5d8510b8a2142be21e203d3594
Asimmons8228 <96853510+Asimmons8228@users.noreply.github.com>
2023-11-29 13:17:35 -0500

    added formulas for essential and non essentials

--- main_app/finhealth.py
+++ main_app/finhealth.py
@@ -5,53 +5,34 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def finhealth_index(request):
   finhealth= FinancialHealth.objects.filter(user=request.user)
-  bills= Bills.objects.filter(user=request.user)
+  bills= Bill.objects.filter(user=request.user)
   monthly_bills = sum(bill.amount for bill in bills)
   yearly_bills = monthly_bills * 12
+  essential_bills = Bill.objects.filter(user = request.user, category='Essential')
+  nonessential_bills = Bill.objects.filter(user = request.user, category = 'Nonessential')
+  total_essential_bills = sum(bill.amount for bill in essential_bills)
+  total_nonessential_bills = sum(bill.amount for bill in nonessential_bills)
 
   income= Income.objects.filter(user=request.user)
-  yearly_income= sum(income.yearly_salary + income.other_income for income in income)
+  yearly_income= sum(income.amount for income in income)
   monthly_income= yearly_income / 12
   rounded_monthly_income = round(monthly_income, 2)
 
-  expenses= Expenses.objects.filter(user=request.user)
+  expenses= Expense.objects.filter(user=request.user)
   total_expenses = sum(expense.amount for expense in expenses)
   yearly_estimated_expenses = total_expenses * 12
+  essential_expenses = Expense.objects.filter(user = request.user, category='Essential')
+  nonessential_expenses = Expense.objects.filter(user = request.user, category = 'Nonessential')
+  total_essential_expenses = sum(expense.amount for expense in essential_expenses)
+  total_nonessential_expenses = sum(expense.amount for expense in nonessential_expenses)
 
   needs_percent = (yearly_bills + yearly_estimated_expenses) / yearly_income * 100
   savings_percent = ((yearly_income - (yearly_bills + yearly_estimated_expenses)) / yearly_income) * 100
-  nonessential_percent = (sum(expense.amount for expense in expenses) / yearly_income) * 100
-  bill_spending_percent = (sum(bill.amount for bill in bills) / yearly_income) * 100
+  nonessential_percent = ((total_nonessential_bills + total_nonessential_expenses) / yearly_income) * 100
+  essential_percent = ((total_essential_bills + total_essential_expenses) / yearly_income) * 100
 
-  if (
-    needs_percent <= 50
-    and savings_percent >= 20
-    and nonessential_percent <= 15
-    and bill_spending_percent <= 30
-  ):
-    financial_health_grade = 'A+'
-  elif (
-    50 < needs_percent <= 55
-    and 17 <= savings_percent < 20
-    and 15 <= nonessential_percent <= 18
-    and 30 < bill_spending_percent <= 35
-  ):
-    financial_health_grade = 'A'
-  elif (
-    55 < needs_percent <=60
-    and 15 <= savings_percent < 17
-    and 18 < nonessential_percent <= 20
-    and 35 < bill_spending_percent <= 40
-  ):
-    financial_health_grade = 'B+'
-  elif(
-    60 < needs_percent <= 65
-    and 13 <= savings_percent < 15
-    and 21 < nonessential_percent <= 18
-    and 40 < bill_spending_percent <= 45
-  ):
-    financial_health_grade = 'B'
-    
+  
+
   return render(request, 'finhealth/index.html', {
     'finhealth': finhealth, 
     'bills': bills, 
@@ -67,7 +48,11 @@ def finhealth_index(request):
     'needs_percent': needs_percent,
     'savings_percent': savings_percent,
     'nonessential_percent': nonessential_percent,
-    'bill_spending_percent': bill_spending_percent,
-    'financial_health_grade': financial_health_grade, 
+    'financial_health_grade': financial_health_grade,
+    'financial_health_score' : financial_health_score,
+    'needs_score' : needs_score ,
+    'savings_score' : savings_score,
+    'nonessential_score' : nonessential_score,
+    'essential_score' : essential_score,
     },
 )
